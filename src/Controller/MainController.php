89478b15752df2f2710a7b124327672ed627ec01
jannisgollwitzer <info@jannisgollwitzer.de>
2021-12-28 20:33:12 +0100

    all

---
+++ src/Controller/MainController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Site;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/", requirements={"route"="^(?!.*api|_wdt|_profiler).+"})
+     */
+    function controller(Request $request, EntityManagerInterface $em, HttpClientInterface $client){
+        /** @var Site $site */
+        $site = $this->getDoctrine()->getRepository(Site::class)->findOneBy(["host" => $request->getHost()]);
+        if($site == null){
+            $site = new Site();
+            $site->setHost("127.0.0.1")->setWebflowAddress("jannis6023.webflow.io");
+        }
+        $response = $client->request($request->getMethod(), "https://" . $site->getWebflowAddress() . "/");
+        $data = $response->getContent();
+        $data = str_replace("<a class='w-webflow-badge' href='https://webflow.com?utm_campaign=brandjs'><img src='https://d3e54v103j8qbb.cloudfront.net/img/webflow-badge-icon.f67cd735e3.svg' alt='' style='margin-right: 8px; width: 16px;'><img src='https://d1otoma47x30pg.cloudfront.net/img/webflow-badge-text.6faa6a38cd.svg' alt='Made in Webflow'></a>", "", $data);
+        return new Response($data, 200, ["Content-Type" => "text/html"]);
+    }
+
+    /**
+     * @Route("/{route}", requirements={"route"="^(?!.*api|_wdt|_profiler).+"})
+     */
+    function controllerROUTE($route, Request $request, EntityManagerInterface $em, HttpClientInterface $client){
+        /** @var Site $site */
+        $site = $this->getDoctrine()->getRepository(Site::class)->findOneBy(["host" => $request->getHost()]);
+        if($site == null){
+            $site = new Site();
+            $site->setHost("127.0.0.1")->setWebflowAddress("jannis6023.webflow.io");
+        }
+        $response = $client->request($request->getMethod(), "https://" . $site->getWebflowAddress() . "/" . $route);
+        $data = $response->getContent();
+        $data = str_replace($site->getWebflowAddress(), $site->getHost(), $data);
+        return new Response($data, 200, ["Content-Type" => "text/html"]);
+    }
+}
---
+++ src/Entity/Site.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SiteRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=SiteRepository::class)
+ */
+class Site
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $host;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $webflowAddress;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getHost(): ?string
+    {
+        return $this->host;
+    }
+
+    public function setHost(string $host): self
+    {
+        $this->host = $host;
+
+        return $this;
+    }
+
+    public function getWebflowAddress(): ?string
+    {
+        return $this->webflowAddress;
+    }
+
+    public function setWebflowAddress(string $webflowAddress): self
+    {
+        $this->webflowAddress = $webflowAddress;
+
+        return $this;
+    }
+}
